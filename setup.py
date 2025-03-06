5568838c5eb3671ccb6fc01ca4bacf4c8c019626
Nitin Varda <nitinvarda@Nitins-Fellow-MacBook-Pro.local>
2025-03-06 14:42:24 -0800

    testing

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages,setup
 from typing import List
 
-
+# testing
 def get_requirements(file_path:str) -> List[str]:
     '''
         This functions gets all the requirements in the form of list
